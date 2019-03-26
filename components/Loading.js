acb86feee34a8f9a601ba7f09ad57ac62f06a02c
Bhavesh Goyal <bhavesh.goyal@vedantu.com>
2019-03-26 12:57:01 +0530

    0.0.4

--- components/Loading.js
+++ components/Loading.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { SkypeIndicator } from 'react-native-indicators';
 import {
   View,
-  Easing,
   StyleSheet,
-  Animated
 } from 'react-native'
 
 const styles = StyleSheet.create({
@@ -20,59 +19,11 @@ const styles = StyleSheet.create({
 })
 
 class Loading extends Component {
-  constructor() {
-    super()
-    this.anim = {
-      width: new Animated.Value(10),
-      translateX: new Animated.Value(-50)
-    }
-  }
-
-  componentDidMount() {
-    Animated.loop(Animated.parallel([
-      Animated.sequence([
-        Animated.timing(this.anim.width, {
-          toValue: 75,
-          easing: Easing.back(1),
-          duration: 750
-        }),
-        Animated.timing(this.anim.width, {
-          toValue: 10,
-          // easing: Easing.back(2),
-          duration: 250
-        }),
-        Animated.timing(this.anim.width, {
-          toValue: 75,
-          easing: Easing.back(1),
-          duration: 750
-        }),
-        Animated.timing(this.anim.width, {
-          toValue: 10,
-          // easing: Easing.back(2),
-          duration: 250
-        })
-      ]),
-      Animated.sequence([
-        Animated.timing(this.anim.translateX, { toValue: 50, easing: Easing.back(1), duration: 1000 }),
-        Animated.timing(this.anim.translateX, { toValue: -50, easing: Easing.back(1), duration: 1000 })
-      ])
-    ])).start()
-  }
-
   render() {
-    const { translateX, width } = this.anim
     if (this.props.loading) {
       return (
         <View style={styles.container}>
-          <Animated.View style={[
-            styles.line,
-            {
-              backgroundColor: this.props.theme,
-              width,
-              transform: [{ translateX }]
-            }
-          ]}
-          />
+          <SkypeIndicator color='white'/> 
         </View>
       )
     }
