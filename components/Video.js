99ad6556f2099ddb27029e25313848d1ca429c5c
Bhavesh Goyal <bhavesh.goyal@vedantu.com>
2019-03-26 12:19:32 +0530

    0.0.3

--- components/Video.js
+++ components/Video.js
@@ -354,7 +354,8 @@ class Video extends Component {
       playInBackground,
       playWhenInactive,
       controlDuration,
-      disableSeek
+      disableSeek,
+      progressUpdateInterval,
     } = this.props
 
     const inline = {
@@ -394,7 +395,7 @@ class Video extends Component {
           muted={muted}
           playInBackground={playInBackground} // Audio continues to play when app entering background.
           playWhenInactive={playWhenInactive} // [iOS] Video continues to play when control or notification center are shown.
-          // progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
+          progressUpdateInterval={progressUpdateInterval}          // [iOS] Interval to fire onProgress (default to ~250ms)
           onLoadStart={() => this.onLoadStart()} // Callback when video starts to load
           onLoad={e => this.onLoad(e)} // Callback when video loads
           onProgress={e => this.progress(e)} // Callback every ~250ms with currentTime
